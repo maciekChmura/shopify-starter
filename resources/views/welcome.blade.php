71327219d5aca9d298635b5e687b5b28d9f5600c
Maciek Chmura <chmura.m@gmail.com>
2024-10-11 11:27:49 +0200

    update Vite config for HTTPS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 @extends('shopify-app::layouts.default')
 
 @section('content')
-    <p>Hello world</p>
+    <p>Hello worlds</p>
+@endsection
+
+@section('scripts')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 @endsection
